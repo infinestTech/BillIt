8a3ed11ab177dbf73fa2be9596a13ae7355819cd
infinestTech <infinest.tech@gmail.com>
2024-11-15 23:31:01 +0530

    Your commit message here

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reactu
+          Learn Reactq
         </a>
       </header>
     </div>
